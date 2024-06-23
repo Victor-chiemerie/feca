b1368cb173c3604dc167faa2b6f535715d8689be
Victor-chiemerie <victorchiemerie02@gmail.com>
2024-06-23 17:25:33 +0100

    setup overview section

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,9 +10,15 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   int _currentIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    List<Widget> newBody = [
+      overViewPage(context),
+      dailyPage(),
+      eventPage(),
+    ];
     return Scaffold(
       backgroundColor: colorScheme.surface,
       drawer: const MyDrawer(),
@@ -48,6 +54,7 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
+      body: newBody[_currentIndex],
       bottomNavigationBar: BottomNavigationBar(
         selectedItemColor: colorScheme.tertiary,
         unselectedItemColor: colorScheme.tertiaryFixedDim,
@@ -78,5 +85,304 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  Widget overViewPage(BuildContext context) {
+    final deviceWidth = MediaQuery.of(context).size.width;
+    final ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+      child: ListView(
+        children: [
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: colorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Add border radius if desired
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  offset: const Offset(2, 3), // Offset the shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ), // shape the image edges
+                    image: DecorationImage(
+                      image: AssetImage("images/overview1.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.5,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "FECA NIGERIA",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Federation of Colleges Ex-Students Christian Association",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: colorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Add border radius if desired
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  offset: const Offset(2, 3), // Offset the shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ), // shape the image edges
+                    image: DecorationImage(
+                      image: AssetImage("images/overview2.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.5,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "BELIEF",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "We are guided by beliefs which act as a compass to fulfilling our mandate",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: colorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Add border radius if desired
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  offset: const Offset(2, 3), // Offset the shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ), // shape the image edges
+                    image: DecorationImage(
+                      image: AssetImage("images/overview3.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.5,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "CORE VALUES",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "Our mission and core values drive our culture and are the foundation of our practice",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: colorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Add border radius if desired
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  offset: const Offset(2, 3), // Offset the shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ), // shape the image edges
+                    image: DecorationImage(
+                      image: AssetImage("images/overview4.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.5,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "ANTHEM",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "We shall rise and shine like the light...",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          const SizedBox(height: 10),
+          Container(
+            height: 130,
+            decoration: BoxDecoration(
+              color: colorScheme.surface,
+              borderRadius:
+                  BorderRadius.circular(10.0), // Add border radius if desired
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.2), // Shadow color
+                  offset: const Offset(2, 3), // Offset the shadow position
+                ),
+              ],
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  height: 130,
+                  width: 130,
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      bottomLeft: Radius.circular(10),
+                    ), // shape the image edges
+                    image: DecorationImage(
+                      image: AssetImage("images/overview5.jpg"),
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  width: deviceWidth * 0.5,
+                  child: const Column(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Text(
+                          "HISTORY",
+                          textAlign: TextAlign.right,
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        "We have a history which act as a reminder to fulfill our vision",
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget dailyPage() {
+    return const Center(child: Text("Daily"));
+  }
+
+  Widget eventPage() {
+    return const Center(child: Text("Events"));
   }
 }
